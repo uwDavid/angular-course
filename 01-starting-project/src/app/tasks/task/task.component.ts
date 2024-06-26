0454c3713417d5404d602b512f7ee775f034fcae
uwDavid <uwDavid@users.noreply.github.com>
2024-06-26 16:14:30 -0400

    template components

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
+  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -9,13 +9,14 @@ import {
 } from '@angular/core';
 
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   //Component Input
