81a60ab6d39b6c7688f3a7e8c4effbd831687809
uwDavid <uwDavid@users.noreply.github.com>
2024-06-27 14:46:53 -0400

    calculate investment

---
+++ 04-financial-calculator/README.md
@@ -0,0 +1,32 @@
+# 4 - Financial Calculator Project
+
+This is a practice project to demo Angular concepts.
+
+### Angular Tips
+
+Angular has a built-in `CurrencyPipe`
+Images stored in `/public` folder works differently from `/assets` folder.
+Reference images in `/public`, do not include the `/public/` prefix:
+
+```html
+<img src="image.png" />
+```
+
+### Typescript Tips
+
+Object types are defined:
+
+```ts
+type MyObj = {
+  someProp1: string;
+  someProp2: number;
+};
+```
+
+Arrays are defined:
+
+```ts
+string[];
+number[];
+MyObj[];
+```
---
+++ 04-financial-calculator/src/app/user-input/user-input.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  // emit calculate event to app component
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    annualInvestment: number;
+    expectedReturn: number;
+  }>();
+
+  onSubmit() {
+    // console.log(this.enteredAnnualInvestment);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+    });
+  }
+}
