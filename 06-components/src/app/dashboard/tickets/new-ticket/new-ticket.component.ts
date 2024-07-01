96a5fb8a347b7cc1825504be44d661474cc767fc
uwDavid <uwDavid@users.noreply.github.com>
2024-07-01 11:48:19 -0400

    template var, viewchild

--- 06-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
-  imports: [ControlComponent],
+  imports: [ControlComponent, FormsModule],
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild<ElementRef<HTMLFormElement>>('form'); // this is a signal
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); // this guarantees element
+  // arg = template variable
+  // ElementRef is a wrapper around the template var -> need to specify underlying type
+  // initially undefined - loads after init
+  onSubmit(title: string, request: string) {
+    // this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
+  }
+}
