b4bda94598a1dc049ad9e1506d749d6554d91c83
uwDavid <uwDavid@users.noreply.github.com>
2024-07-02 16:40:44 -0400

    ticket details feature

--- 06-components/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 06-components/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
+  // collapse ticket feature
+  detailsVisible = signal(false);
+  onToggleDetails() {
+    // Method 1
+    // this.detailsVisible.set(!this.detailsVisible());
+    // note: this won't set up a subcription
+    // to do that we use the effect()
+    // Method 2
+    // update() automatically pass in the previous state as arg
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
--- 06-components/src/app/dashboard/tickets/tickets.component.ts
+++ 06-components/src/app/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
 })
 export class TicketsComponent {
   tickets: Ticket[] = [];
