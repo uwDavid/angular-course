dc1b47868225334e55f54c27177084fd36ba02c1
uwDavid <uwDavid@users.noreply.github.com>
2024-07-02 23:46:39 -0400

    model 2-way binding

--- 06b-custom-2-way/src/app/rect/rect.component.ts
+++ 06b-custom-2-way/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, model, Output } from "@angular/core";
 
 @Component({
     selector: "app-rect",
@@ -8,20 +8,33 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     styleUrl: "./rect.component.css",
 })
 export class RectComponent {
+    // Method 1 - Input Output
     // to enable custom 2-way binding
-    @Input({ required: true }) size!: { width: string; height: string };
+    // @Input({ required: true }) size!: { width: string; height: string };
 
     // onReset - we want to set the size back to standard size
     // but, we can't just do this.size = {} ; b/c size is just an input
     // we need 2-way binding
     // we do this by using an Output
     // Note: 2-way binding is essentially input+output
-    @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
     // Note: this is a special naming convention for Angular
     // tells Angular that these 2 are a pair - Angular does the heavy lifting
     // this Output is to change the value in the parent component
+    // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+
+    // onReset() {
+    //     this.sizeChange.emit({
+    //         width: "200",
+    //         height: "100",
+    //     });
+    // }
+
+    // Angular 17.2+
+    // size = a ModelSignal
+    size = model.required<{ width: string; height: string }>();
+
     onReset() {
-        this.sizeChange.emit({
+        this.size.set({
             width: "200",
             height: "100",
         });
