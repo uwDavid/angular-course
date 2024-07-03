1d03debbc0f508b9a180cbdc41c28b03504e98f0
uwDavid <uwDavid@users.noreply.github.com>
2024-07-02 23:42:30 -0400

    input output 2-way binding

--- 06b-custom-2-way/src/app/rect/rect.component.ts
+++ 06b-custom-2-way/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
     selector: "app-rect",
@@ -11,7 +11,19 @@ export class RectComponent {
     // to enable custom 2-way binding
     @Input({ required: true }) size!: { width: string; height: string };
 
+    // onReset - we want to set the size back to standard size
+    // but, we can't just do this.size = {} ; b/c size is just an input
+    // we need 2-way binding
+    // we do this by using an Output
+    // Note: 2-way binding is essentially input+output
+    @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+    // Note: this is a special naming convention for Angular
+    // tells Angular that these 2 are a pair - Angular does the heavy lifting
+    // this Output is to change the value in the parent component
     onReset() {
-        // ...
+        this.sizeChange.emit({
+            width: "200",
+            height: "100",
+        });
     }
 }
