40532ee309a7d66eaa5f765fc598b09867405c7b
uwDavid <uwDavid@users.noreply.github.com>
2024-07-05 17:24:49 -0400

    add destroy ref

--- 12-http-frontend/src/app/places/available-places/available-places.component.ts
+++ 12-http-frontend/src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { subscribeOn } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -12,7 +13,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
 
   // Send HTTP request to backend
@@ -20,4 +21,26 @@ export class AvailablePlacesComponent {
   // note: we need to set up a provider as well
   // we can use the providers:
   // or we can set it up at the root level
+
+  private destroyRef = inject(DestroyRef);
+
+  // Fetch data
+  ngOnInit() {
+    // populate data when component is ready
+    // .get() returns an observable
+    // we need to subscribe() to trigger the GET request
+    // and then pass an observer function to handle the response
+    const subscription = this.httpClient
+      .get('http://localhost:3000/places')
+      .subscribe({
+        next: (resData) => {
+          console.log(resData);
+        },
+      });
+
+    // good practice to clean up subscription
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
