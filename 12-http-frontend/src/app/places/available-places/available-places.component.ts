54a5138ad9882c29c8b71f21d29a987ed9088a9e
uwDavid <uwDavid@users.noreply.github.com>
2024-07-05 17:12:36 -0400

    set up http client

--- 12-http-frontend/src/app/places/available-places/available-places.component.ts
+++ 12-http-frontend/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
@@ -13,4 +14,10 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent {
   places = signal<Place[] | undefined>(undefined);
+
+  // Send HTTP request to backend
+  private httpClient = inject(HttpClient);
+  // note: we need to set up a provider as well
+  // we can use the providers:
+  // or we can set it up at the root level
 }
