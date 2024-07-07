932727a49530390134567b09330a2ab316c05466
uwDavid <uwDavid@users.noreply.github.com>
2024-07-06 22:08:50 -0400

    http interceptors

--- 12-http-frontend/src/main.ts
+++ 12-http-frontend/src/main.ts
@@ -1,8 +1,41 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
 
+// a custom interceptor function
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  // to change the header
+  // const req = request.clone({
+  //   headers: request.headers.set('X-DEBUG', 'TESTING'), // set() copies all previous headers & adds this one
+  // });
+  console.log('[Outgoing Request]');
+  console.log(request);
+  // to intercept responses
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
+}
+
+// register Http interceptor
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch((err) => console.error(err));
