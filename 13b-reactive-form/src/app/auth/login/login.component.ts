203e35910a70fde922a2b710014ba38c4e0911cd
uwDavid <uwDavid@users.noreply.github.com>
2024-07-06 22:22:04 -0400

    reactive for initial setup

--- 13b-reactive-form/src/app/auth/login/login.component.ts
+++ 13b-reactive-form/src/app/auth/login/login.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    // key-value pair = one control || nested FormGroup
+    email: new FormControl(''), // empty initial value
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    this.form.value.email; // access form value
+  }
+}
