5370b30089efa83725df5e0829a0e8c1a5afc760
uwDavid <uwDavid@users.noreply.github.com>
2024-07-10 17:46:11 -0400

    adding reference links

--- 14-routing/app.routes.ts
+++ 14-routing/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './src/app/tasks/tasks.component';
+import { NoTaskComponent } from './src/app/tasks/no-task/no-task.component';
 
 export const routes: Routes = [
   {
-    path: 'task', // <domain-url>/tasks
+    path: '', // <domain-url>/
+    component: NoTaskComponent,
+  },
+  {
+    path: 'tasks', // <domain-url>/tasks
     component: TasksComponent,
   },
 ];
