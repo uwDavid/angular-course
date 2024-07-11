26490e8caf1effac55905b3df51b1828373a689a
uwDavid <uwDavid@users.noreply.github.com>
2024-07-11 14:53:20 -0400

    nested route and relative links

--- 14-routing/app.routes.ts
+++ 14-routing/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TasksComponent } from './src/app/tasks/tasks.component';
 import { NoTaskComponent } from './src/app/tasks/no-task/no-task.component';
 import { UserTasksComponent } from './src/app/users/user-tasks/user-tasks.component';
+import { NewTaskComponent } from './src/app/tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,16 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <domain-url>/users/<uid>
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', //<domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
   // {
   //   path: 'tasks', // <domain-url>/tasks
--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,11 +1,13 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink],
 })
 export class UserTasksComponent {
   userId = input.required<string>();
