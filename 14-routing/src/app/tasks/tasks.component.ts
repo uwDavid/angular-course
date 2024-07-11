a98471664956501e953952b87e07c26db9dc13ba
uwDavid <uwDavid@users.noreply.github.com>
2024-07-11 15:03:33 -0400

    route param for nested route

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -11,5 +12,11 @@ import { Task } from './task/task.model';
   imports: [TaskComponent],
 })
 export class TasksComponent {
-  userTasks: Task[] = [];
+  userId = input.required<string>();
+  // for this to work - we need to set paramsInheritanceStrategy: 'always'
+  // b/c it's a nested route
+  private tasksService = inject(TasksService);
+  userTasks = computed(() =>
+    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+  );
 }
