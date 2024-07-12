138037c3cb1dc1a4d730521138f7d97245e6ac11
uwDavid <uwDavid@users.noreply.github.com>
2024-07-12 17:05:57 -0400

    task sorting

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -5,6 +5,7 @@ import {
   inject,
   input,
   OnInit,
+  signal,
 } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
@@ -25,19 +26,31 @@ export class TasksComponent implements OnInit {
   // for this to work - we need to set paramsInheritanceStrategy: 'always'
   // b/c it's a nested route
   private tasksService = inject(TasksService);
+
+  order = signal<'asc' | 'desc'>('desc');
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'desc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
   );
 
   // order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc';
-  // extra query param via Observables
+  // order?: 'asc' | 'desc';
+
+  // extract query param via Observables
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: (params) => (this.order = params['order']),
+      next: (params) => this.order.set(params['order']),
     });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
