37e62c6b41e0cb5d964475883092b36adb9e5bf8
uwDavid <uwDavid@users.noreply.github.com>
2024-07-17 17:32:01 -0400

    route guard

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
@@ -7,6 +7,20 @@ import {
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  // return boolean (true / false) or Observable that returns boolean
+  const router = inject(Router);
+
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+  // return false;
+  //this breaks the app => we should re-direct
+};
 
 export const routes: Routes = [
   {
@@ -17,6 +31,8 @@ export const routes: Routes = [
     path: 'users/:userId', // <domain-url>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
+    // again we only point at the Guard func
     data: {
       message: 'hello',
     },
