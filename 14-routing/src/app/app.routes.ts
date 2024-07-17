a3952e03834930de0fa4a1208ca47ab0e186951f
uwDavid <uwDavid@users.noreply.github.com>
2024-07-17 15:56:55 -0400

    router resolved data

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
@@ -13,6 +16,14 @@ export const routes: Routes = [
     path: 'users/:userId', // <domain-url>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+    data: {
+      message: 'hello',
+    },
+    resolve: {
+      userName: resolveUserName,
+      // note: we don't execute resolveUserName(), instead we just point at the resolver function
+      // Angular will execute it for us
+    },
   },
   {
     // fallback catch-all route
--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,12 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -10,10 +16,34 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   imports: [RouterOutlet, RouterLink],
 })
 export class UserTasksComponent {
-  userId = input.required<string>();
-  private userServce = inject(UsersService);
+  // userId = input.required<string>();
+  // private userServce = inject(UsersService);
 
-  userName = computed(
-    () => this.userServce.users.find((u) => u.id === this.userId())?.name
-  );
+  // Route data
+  message = input.required<string>();
+
+  // ngOnInit(): void {
+  //   console.log('Input data defined in route: ' + this.message());
+  // }
+  // userName = computed(
+  //   () => this.userServce.users.find((u) => u.id === this.userId())?.name
+  // );
+
+  // using Dynamic Route Data
+  userName = input.required<string>();
 }
+
+// export function resolveUserName(){}
+// we use the below syntax to give it at type to the constant
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const userService = inject(UsersService);
+  const userName =
+    userService.users.find(
+      (u) => u.id === activatedRoute.paramMap.get('userId')
+    )?.name || '';
+
+  return userName;
+};
